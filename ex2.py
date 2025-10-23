113d09aa0683147503b62ae17d1b2e109267eb2d
Ramona Enzi <ramona.enzi@edu.fh-joanneum.at>
2025-10-23 10:23:26 +0200

    added exercise 6 and 7

--- ex2.py
+++ ex2.py
@@ -11,3 +11,11 @@ print(college)
 
 college['Elite'] = pd.cut(college['Top10perc'], bins=[0, 50, 100], labels=['Nicht-Elite', 'Elite'])
 print(college[['Top10perc', 'Elite']])
+
+by_private = (college.groupby("Private", observed=True)[["Acceptance_Rate", "Room_Board", "Books", "PhD", "Grad_Rate"]].mean().round(2))
+print("\n=== Means grouped by Private ===")
+print(by_private)
+
+by_private_elite = (college.groupby(["Private", "Elite"], observed=True)[["Acceptance_Rate", "Room_Board", "Books", "PhD", "Grad_Rate"]].mean().round(2))
+print("\n=== Means grouped by Private and Elite ===")
+print(by_private_elite)
