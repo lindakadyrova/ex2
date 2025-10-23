37356447c6bfb4f4e62eedc2bdd73819f685716f
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-10-23 14:58:53 +0200

    added solution for the fifth question

--- ex2.py
+++ ex2.py
@@ -9,8 +9,13 @@ college.sort_values(by=['Private', 'Apps'], ascending=[True, True], inplace=True
 print(college)
 
 
+
 college['Elite'] = pd.cut(college['Top10perc'], bins=[0, 50, 100], labels=['Nicht-Elite', 'Elite'])
 print(college[['Top10perc', 'Elite']])
+
+print(college.describe())
+
+
 
 by_private = (college.groupby("Private", observed=True)[["Acceptance_Rate", "Room_Board", "Books", "PhD", "Grad_Rate"]].mean().round(2))
 print("\n=== Means grouped by Private ===")
